6559b964ca831f5420e18ea274fefe1910d9ff84
AlexMerinov <merinovaleks1922@gmail.com>
2022-11-23 17:10:07 +0300

    main page

--- src/scripts/blocks/sliders.js
+++ src/scripts/blocks/sliders.js
@@ -1,10 +1,25 @@
-// window.app.Sliders = () => {
+window.app.Sliders = () => {
 
-//    new Swiper('.go-js-banner-slider', {
-//       slidesPerView: 1,
-//       spaceBetween: 20,
-//    });
+   new Swiper('.banner-slider', {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
 
-// };
+      breakpoints: {
+         768: {
+            slidesPerView: 1.2,
+            spaceBetween: 20,
+         },
 
-// window.app.Sliders();
+         1360: {
+            slidesPerView: 1,
+            pagination: {
+               el: '.banner-slider__pagination',
+               clickable: true,
+             },
+         },
+      }
+   });
+
+};
+
+window.app.Sliders();
