0f5772545e726405d875b9af7d5023c23dcd4b21
AlexMerinov <merinovaleks1922@gmail.com>
2022-11-24 15:14:37 +0300

    init sliders

--- src/scripts/blocks/sliders.js
+++ src/scripts/blocks/sliders.js
@@ -1,6 +1,6 @@
 window.app.Sliders = () => {
 
-   new Swiper('.banner-slider', {
+   const sliderBanner = new Swiper('.banner-slider', {
       slidesPerView: 1.1,
       spaceBetween: 10,
 
@@ -19,6 +19,132 @@ window.app.Sliders = () => {
          },
       }
    });
+
+   const initSliders = () => {
+
+      const sliders = document.querySelectorAll('.go-js-slider');
+      if (!sliders) return;
+
+      sliders.forEach((slider) => {
+         
+         const sliderBest = slider.querySelector('.slider-best');
+         const sliderView = slider.querySelector('.slider-view');
+         const navArrows = {
+            navigation: {
+               nextEl: slider.closest('.section').querySelector('.go-js-slider__btn--next'),
+               prevEl: slider.closest('.section').querySelector('.go-js-slider__btn--prev'),
+            }
+         }
+
+         switch (true) {
+            case slider.dataset.sliderName == 'best':
+               new Swiper(sliderBest, {
+                  slidesPerView: 2.2,
+                  spaceBetween: 10,
+                  breakpoints: {
+                     768: {
+                        slidesPerView: 2.6,
+                        spaceBetween: 20,
+                     },
+                     1360: {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                        navigation: {
+                           nextEl: navArrows.navigation.nextEl,
+                           prevEl: navArrows.navigation.prevEl,
+                        },
+                     },
+                  }
+               }); 
+               break;
+            case slider.dataset.sliderName == 'view':
+               new Swiper(sliderView, {
+                  slidesPerView: 1.2,
+                  spaceBetween: 10,
+                  loop: true,
+                  breakpoints: {
+                     768: {
+                        slidesPerView: 2.5,
+                        spaceBetween: 20,
+                     },
+                     1360: {
+                        slidesPerView: 3,
+                        navigation: {
+                           nextEl: navArrows.navigation.nextEl,
+                           prevEl: navArrows.navigation.prevEl,
+                        },
+                     }
+                  }
+               });
+               break;
+         }
+      });
+
+   };
+
+   initSliders();
+
+
+
+   // const sliders = document.querySelectorAll('.section .slider-best');
+
+   // if (sliders) {
+   //    sliders.forEach(function (slider) {
+   //       const navArrows = {
+   //          navigation: {
+   //             nextEl: slider.closest('.section').querySelector('.slider-best__btn--next'),
+   //             prevEl: slider.closest('.section').querySelector('.slider-best__btn--prev'),
+   //          }
+   //       }
+
+   //       new Swiper(slider, {
+
+   //          slidesPerView: 2.2,
+   //          spaceBetween: 10,
+         
+   //          breakpoints: {
+   //             768: {
+   //                slidesPerView: 2.6,
+   //                spaceBetween: 20,
+   //             },
+         
+   //             1360: {
+   //                slidesPerView: 3,
+   //                spaceBetween: 30,
+
+   //                navigation: {
+   //                   nextEl: navArrows.navigation.nextEl,
+   //                   prevEl: navArrows.navigation.prevEl,
+   //                },
+   //             },
+   //          },
+         
+   //       });      
+
+   //    });
+   // }
+
+   // const sliderView = new Swiper('.slider-view', {
+   //    slidesPerView: 1.1,
+   //    spaceBetween: 10,
+   //    loop: true,
+
+   //    breakpoints: {
+   //       768: {
+   //          slidesPerView: 2.5,
+   //          spaceBetween: 20,
+   //       },
+
+   //       1360: {
+   //          slidesPerView: 3,
+   //          spaceBetween: 30,
+   //          navigation: {
+   //             nextEl: '.slider-best__btn--next',
+   //             prevEl: '.slider-best__btn--prev',
+   //          }
+   //       },
+   //    }
+   // });
 
 };
 
